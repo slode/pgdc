bf5839570cb789a01b31a8ba185c6cdfe5c433b4
Stian Lode <stian.lode@boost.ai>
2022-05-20 12:19:24 +0200

    Removes un-necessary session from sqlbuilder tests.

--- tests/test_sqlbuilder.py
+++ tests/test_sqlbuilder.py
@@ -1,53 +1,33 @@
 import tests.ctx
 import asyncio
 
-from pgdc import AsyncpgSession, Where, Limit, SqlSelect, SqlInsert, SqlUpdate, SqlDelete, render
+from pgdc import Where, Limit, SqlSelect, SqlInsert, SqlUpdate, SqlDelete, render
 
 async def test_insert():
-    db = AsyncpgSession()
-    await db.connect()
-
     builder = SqlInsert(
         "search_keys", ("id", "key", "date_created"), key="new-key-text"
     )
     query, args = render(builder)
     print(query, args)
 
-    await db.close()
-
 
 async def test_select():
-    db = AsyncpgSession()
-    await db.connect()
-
     builder = SqlSelect(
         "search_keys", ("id", "key", "date_created"), Where(id=1), Limit(1)
     )
     query, args = render(builder)
     print(query, args)
 
-    await db.close()
-
 async def test_update():
-    db = AsyncpgSession()
-    await db.connect()
-
     builder = SqlUpdate(
         "search_keys", ("id", "key", "date_created"), Where(id=1), key="updated-key-text"
     )
     query, args = render(builder)
     print(query, args)
 
-    await db.close()
-
 async def test_delete():
-    db = AsyncpgSession()
-    await db.connect()
-
     builder = SqlDelete(
         "search_keys", Where(id=1)
     )
     query, args = render(builder)
     print(query, args)
-
-    await db.close()
